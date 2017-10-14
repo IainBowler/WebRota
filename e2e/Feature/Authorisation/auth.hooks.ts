05ffd74012e6e2da5ea44af4157de60d9b700ca8
IainBowler <git@jokedb.net>
2017-10-14 12:40:03 +0100

    Added a couple of Home page scenarios with new loggedOn tag.

--- e2e/Feature/Authorisation/auth.hooks.ts
+++ e2e/Feature/Authorisation/auth.hooks.ts
@@ -1,3 +1,4 @@
+import { AppPage } from '../App/App.po';
 import { AuthHelper } from "./AuthHelper";
 
 var {defineSupportCode} = require('cucumber');
@@ -5,6 +6,7 @@ var {defineSupportCode} = require('cucumber');
 defineSupportCode(function({After, Before, registerHandler}) {
 
   var authHelper = new AuthHelper();
+  var appPage = new AppPage();
 
   registerHandler('BeforeFeatures', () => {
     return authHelper.addUser('existinguser@jokedb.net', 'existinguser');
@@ -13,6 +15,11 @@ defineSupportCode(function({After, Before, registerHandler}) {
   After({tags: "@newuser"}, () => {
     // This hook will be executed after scenarios tagged with @newuser
     return authHelper.deleteUser('newuser@jokedb.net');
+  });  
+
+  Before({tags: "@loggedOn"}, () => {
+    // This hook will be executed before scenarios tagged with @loggedOn
+    return appPage.LogoutLogin('existinguser@jokedb.net', 'existinguser');
   });  
 
   registerHandler('AfterFeatures', () => {
--- e2e/Feature/Home/Home.steps.ts
+++ e2e/Feature/Home/Home.steps.ts
@@ -2,24 +2,39 @@ import { HomePage } from './Home.po';
 import { browser, by, element } from 'protractor';
 import { expect } from 'chai';
 
+var {defineSupportCode} = require('cucumber');
+
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 
 chai.use(chaiAsPromised);
 
-module.exports = function () {
+defineSupportCode(function({Given, Then, When}) {
 
   var page = new HomePage();
 
-  this.Given(/^I am on my home page$/, function () {
+  Given(/^I am on my home page$/, {timeout: 20 * 1000}, () => {
 
     return page.navigateTo();
 
   });
 
-  this.Then(/^Page should display welcome message$/, function () {
+  Given(/^I am not the owner or part of an organisation$/, () => {
+    //Don't have to do anything.
+    return true;
+    
+  });
+    
+  Then(/^Page should display welcome message$/, () => {
+    
+    return page.checkThisIsUsersHomePage('existinguser@jokedb.net')
+    
+  });
+        
 
-    //return expect(page.getWelcomeText()).to.eventually.include('Welcome to ');
+  Then(/^Page should prompt me to join or create an organisation$/, () => {
+
+    return Promise.resolve('pending');
 
   });
-};
+});
