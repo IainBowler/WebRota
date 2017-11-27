c686fa6e0886920bd463e05fb9fd3c6a9255969b
IainBowler <git@jokedb.net>
2017-11-27 12:39:38 +0000

    Added constructor to Member.

--- src/app/Data/member.ts
+++ src/app/Data/member.ts
@@ -4,4 +4,9 @@ export class Member {
   name: string;
   userId: string;
 
+  constructor(name:string = '', userId:string = '', id:number = 0) {
+    this.name = name;
+    this.userId = userId;
+    this.id = id;
+  }
 }
